659d29c836d29b5de92613372a9f6500e9745e82
KwonH <doublus@gmail.com>
2014-03-23 19:47:45 +0900

    부족한 부분 좀더 보완하여 UP

--- login.py
+++ login.py
@@ -87,9 +87,8 @@ class UserController:
         if 'remember_me' in request.form:
             remember_me = True
 
-        #registered_user = User.query.filter_by(user_id=user_id).first()
         registered_user = db_session.query(User).filter(User.user_id==user_id).first()
-        #User.query.filter_by(user_id=user_id).first()
+        db_session.close()
 
         if registered_user is None:
             flash('Username is invalid' , 'error')
@@ -102,24 +101,26 @@ class UserController:
         login_user(registered_user, remember = remember_me)
         flash('Logged in successfully')
 
-        return redirect(request.args.get('next') or url_for('show_entries'))
+        return redirect(url_for('show_entries'))
 
     @app.route('/user/logout')
     def logout():
         print 'Log Out'
-        '''
-        session.pop('user_id', None)
-        session.pop('logged_in', None)
-        '''
+
         logout_user()
         return redirect(url_for('show_entries'))
 
+    '''
     @app.route('/user/add')
     def add():
         return render_template('user/add.html')
+    '''
 
-    @app.route('/user/add', methods=['POST'])
-    def add_entry():
+    @app.route('/user/add', methods=['GET','POST'])
+    def add():
+        if request.method == 'GET':
+            return render_template('user/add.html')
+
         u = User(request.form['user_id'], request.form['nickname'], request.form['password'])
         db_session.add(u)
         db_session.commit()
@@ -127,33 +128,32 @@ class UserController:
         db_session.close()
         return redirect(url_for('show_entries'))
 
-    @app.route('/user/update')
+    @app.route('/user/update/<user_id>', methods=['GET','POST'])
     @login_required
-    def update():
-        print 'update !!'
-        user = db_session.query(User).filter(User.user_id==request.args.get('user_id')).first()
+    def update(user_id):
+        registered_user = db_session.query(User).filter(User.user_id==user_id).first()
         db_session.close()
-        return render_template('user/update.html', user_id = user.user_id , nickname=user.nickname )
 
-    @app.route('/user/update', methods=['POST'])
-    @login_required
-    def update_entry():
+        if request.method == 'GET':
+            return render_template('user/update.html', user=registered_user)
+
         if request.form['user_id'] == g.User.user_id:
-            u = db_session.query(User).filter(User.user_id==request.form['user_id']).first()
+            registered_user = db_session.query(User).filter(User.user_id==request.form['user_id']).first()
         
-            u.nickname = request.form['nickname']
-            u.password = request.form['password']
+            registered_user.nickname = request.form['nickname']
+            registered_user.set_password( request.form['password'] )
 
             db_session.commit()
             db_session.close()
+            return redirect(url_for('show_entries'))
 
         flash('You are not authorized to edit this todo item','error')
 
         return redirect(url_for('show_entries'))
 
-    @app.route('/user/delete', methods=['GET'])
-    def delete_entry():
-        user_id = request.args.get('user_id')
+    @app.route('/user/delete/<user_id>', methods=['GET'])
+    @login_required
+    def delete(user_id):
         print 'id : ' + user_id
         db_session.query(User).filter(User.user_id==user_id).delete()
         db_session.commit()
